38bdb55dfa8a9189b49300b5b9f831d2d8d2ab36
k1d12r04 <k1d12r04@gmail.com>
2024-10-06 15:54:17 +0300

    Change display format of date and time, types and errors

--- src/components/PharmacyList.tsx
+++ src/components/PharmacyList.tsx
@@ -3,17 +3,16 @@ import { Loader } from '@mantine/core';
 import { FaLocationDot } from 'react-icons/fa6';
 import { FaSquarePhone } from 'react-icons/fa6';
 
-const PharmacyList: React.FC<{
-  province: string | null;
-  district: string | null;
-}> = ({ province, district }) => {
-  const { data, error, isLoading } = usePharmaciesOnDuty(
-    province || '',
-    district || ''
-  );
+type PharmacyListProps = {
+  province: string;
+  district: string;
+};
+
+const PharmacyList = ({ province, district }: PharmacyListProps) => {
+  const { data, error, isLoading } = usePharmaciesOnDuty(province, district);
 
   if (!province || !district) {
-    return;
+    return null;
   }
 
   if (isLoading)
@@ -22,31 +21,62 @@ const PharmacyList: React.FC<{
         <Loader color="blue" size="sm" type="bars" />
       </div>
     );
-  if (error) return <div>An error occurred: {error.message}</div>;
+  if (error)
+    return (
+      <div className="w-2/3 mx-auto mt-10 flex justify-center">
+        <h3>Bir hata oluştu: {error.message}</h3>
+      </div>
+    );
+
+  if (!data || data.length < 1)
+    return (
+      <div className="w-2/3 mx-auto mt-10 flex justify-center">
+        <h3>Maalesef sonuç bulunamadı...</h3>
+      </div>
+    );
 
   return (
     <ul className="w-2/3 mx-auto p-10 mt-10 text-neutral-200 grid grid-cols-2 gap-14">
-      {data?.map(pharmacy => (
-        <li
-          key={pharmacy.pharmacyName}
-          className="bg-blue-700 p-8 rounded-md grid gap-3"
-        >
-          <h4 className="text-xl font-bold">{pharmacy.pharmacyName}</h4>
-          <div className="pharmacy-data-body-text">
-            <FaLocationDot className="text-2xl" /> <p>{pharmacy.address}</p>
-          </div>
-          <div className="pharmacy-data-body-text">
-            <FaSquarePhone className="text-2xl" />
-            <p>{pharmacy.phone}</p>
-          </div>
-          <div className="text-sm">
-            Nöbet başlama tarihi ve saati: <p>{pharmacy.pharmacyDutyStart}</p>
-          </div>
-          <div className="text-sm">
-            Nöbet bitiş tarihi ve saati: <p>{pharmacy.pharmacyDutyEnd}</p>
-          </div>
-        </li>
-      ))}
+      {data?.map(pharmacy => {
+        const dutyStartDate = new Date(pharmacy.pharmacyDutyStart);
+        const dutyStartDateString = dutyStartDate.toLocaleDateString();
+        const dutyStartTimeString = dutyStartDate.toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+        });
+        const dutyEndDate = new Date(pharmacy.pharmacyDutyEnd);
+        const dutyEndDateString = dutyEndDate.toLocaleDateString();
+        const dutyEndTimeString = dutyEndDate.toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+        });
+
+        return (
+          <li
+            key={pharmacy.pharmacyName}
+            className="bg-blue-700 p-8 rounded-md grid gap-3"
+          >
+            <h4 className="text-xl font-bold">{pharmacy.pharmacyName}</h4>
+            <div className="pharmacy-data-body-text">
+              <FaLocationDot className="text-2xl" /> <p>{pharmacy.address}</p>
+            </div>
+            <div className="pharmacy-data-body-text">
+              <FaSquarePhone className="text-2xl" />
+              <p>{pharmacy.phone}</p>
+            </div>
+
+            <p className="text-sm">
+              Nöbet başlangıç tarihi ve saati: {dutyStartDateString} -{' '}
+              {dutyStartTimeString}
+            </p>
+
+            <p className="text-sm">
+              Nöbet bitiş tarihi ve saati: {dutyEndDateString} -{' '}
+              {dutyEndTimeString}
+            </p>
+          </li>
+        );
+      })}
     </ul>
   );
 };
